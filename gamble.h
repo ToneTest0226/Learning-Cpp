4268bbab003c971cc0237a75a659b4e7cc0e7102
ToneTest <160059734+ToneTest0226@users.noreply.github.com>
2024-11-12 12:58:06 -0900

    switching logic to a header instead of one file

---
+++ gamble.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unistd.h>
+#include <memory>
+#include <any>
+#include <string> 
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include <cmath>
+
+int randomnumber;
+extern double money;   
+extern int cycleamnt;
+class Gamble{
+
+public:
+
+static int randomnum(){
+    money - 10;
+ do{
+  usleep(500000);
+  system("clear");
+  srand(time(0));
+  randomnumber = rand() % 100 + 1;
+  std::cout << randomnumber << '\n';
+  --cycleamnt;
+  } while(cycleamnt != 0);
+ return 0;
+};
+
+
+
+};
--- randomnumber.cpp
+++ randomnumber.cpp
@@ -1,6 +1,30 @@
+#include "gamble.h"
 #include <iostream>
 #include <unistd.h>
 #include <memory>
 #include <any>
 #include <string> 
 #include <cstdlib>
+#include <bits/stdc++.h>
+#include <cmath>
+
+
+double money = 100;
+int cycleamnt = 10;
+int guessnum;
+
+int guess(){
+  std::cout << "Take a guess" << '\n';
+  std::cin >> guessnum;
+  return std::round(guessnum);
+  Gamble::randomnum();
+    return 0;
+ }
+
+int distance();
+
+
+int main(){
+guess();    
+return 0;
+}
