c842e472ad8c5ca511e25188558305c1f2ee72d1
ToneTest <160059734+ToneTest0226@users.noreply.github.com>
2025-02-05 08:52:12 -0900

    couldnt do it :(

--- Brain Dead Projects for School/Paths/Paths.cpp
+++ Brain Dead Projects for School/Paths/Paths.cpp
@@ -8,11 +8,22 @@
 //1  = orange, end square 
 //2 = red, blocked square 
 //3 = green, start square
+//4 = Blue, Path taken
 int checkarray = 0;
 int isstart;
 int isend;
-int row;
-
+bool starttopleft = false;
+bool firstrun = true;
+int startpos;
+int endpos;
+void checkshortestpath();
+int currentpos;
+int total_length;
+    Color::Modifier def(Color::FG_DEFAULT);
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier green(Color::FG_GREEN);
+    Color::Modifier blue(Color::FG_BLUE);
+    Color::Modifier orange(Color::FG_ORANGE);
 
 int Path[25] = {
     1,0,0,0,0,
@@ -23,37 +34,83 @@ int Path[25] = {
             };
 
 void sayit(){
-Color::Modifier def(Color::FG_DEFAULT);
-Color::Modifier red(Color::FG_RED);
-Color::Modifier green(Color::FG_GREEN);
-Color::Modifier blue(Color::FG_ORANGE);
-  while(checkarray != 25){
-   if(checkarray % 5 == 0 && checkarray != 0 || checkarray % 5 == 5){
-     std::cout << "\n"; 
-     row++;
+  int getlength = sizeof(Path) / sizeof(Path[0]); 
+  total_length = getlength - 1;
+  
+  //std::cout << total_length;
+  checkarray = 0;
+  if (firstrun == true){
+    while(checkarray != 25){
+     if(checkarray % 5 == 0 && checkarray != 0 || checkarray % 5 == 5){
+      }
+      if(Path[checkarray] == 1){ //end
+       isend = checkarray;
+      }
+      if(Path[checkarray] == 3){ //start
+        isstart = checkarray;
+      }
+        checkarray++;
     }
-    if(Path[checkarray] == 0){ //empty
-     std::cout << def << Path[checkarray];
-    };
-    if(Path[checkarray] == 1){ //end
-     std::cout << blue << Path[checkarray];
-     isend++;
-    };
-    if(Path[checkarray] == 2){ //blocked
-     std::cout << red << Path[checkarray];
-    };
-    if(Path[checkarray] == 3){ //start
-     std::cout << green << Path[checkarray];
-     isstart++;
-    };
+  }else if(firstrun == false){
+    checkarray = 0;
+    while(checkarray != 25){
+     if(checkarray % 5 == 0 && checkarray != 0 || checkarray % 5 == 5){
+      std::cout << "\n"; 
+      }
+     if(Path[checkarray] == 0){ //empty
+       std::cout << def << Path[checkarray];
+      }
+     if(Path[checkarray] == 1){ //end
+       std::cout << orange << Path[checkarray];
+      }
+     if(Path[checkarray] == 2){ //blocked
+       std::cout << red << Path[checkarray];
+      }
+     if(Path[checkarray] == 3){ //start
+       std::cout << green << Path[checkarray];
+      }
+     if(Path[checkarray] == 4){ //path
+       std::cout << blue << Path[checkarray];
+      }
     checkarray++;
-  };
+  }
+  }else{
+    std::cout << "-------------------- SOME TING WONG! --------------------"; 
+  }
+  //std::cout << "\n";
+ //std::cout << total_length;
+    if(firstrun == true){
+        checkshortestpath();
+    }else{
+        std::cout << "\n";
+        exit(0);
+    }
+}
 
-};
+
+void pathfound(){
+    if(Path[isend + 1] = 4){
+      // currentpos - 1;
+      // Path[currentpos] = 4;
+       // std::cout << "Path found!";
+        firstrun = false;   
+        sayit();
+    }else if(Path[total_length -1] = 4){
+       // currentpos + 1;       
+       // Path[currentpos] = 4;
+        //std::cout << "Path found!";
+        firstrun = false;   
+        sayit();
+        exit(0);
+        }
+}
 
 void checkshortestpath(){
+    Path[total_length - 1] = 4;
+    firstrun = false;
+    sayit();
+}
 
-};
 
 int main(){
     sayit();
